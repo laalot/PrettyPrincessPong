42d28fc1abff887a177a269767e8df0076a32b33
Asaf Efraim <asafefraim@gmail.com>
2020-05-30 19:28:13 +0100

    Give ball directional impulse when hits paddle

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -9,6 +9,8 @@ public class Ball : MonoBehaviour
     public float ballSpeed = 2.5f;
     public float maxBallSpeed = 100.0f;
 
+    public float batImpulseMagnitude = 10.0f;
+
     private Vector2 movementVector;
 
     private ScoreTracker scoreTracker;
@@ -65,16 +67,31 @@ public class Ball : MonoBehaviour
         rb2d.AddForce(movementVector * ballSpeed, ForceMode2D.Impulse);
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Paddle"))
+        {
+            var collisionPoint = collision.GetContact(0).point;
+            var distanceFromPaddleCentre = collisionPoint.y - collision.collider.attachedRigidbody.position.y; // positive or negative
+            var collisionPointPortion = distanceFromPaddleCentre / collision.collider.bounds.size.y;
+            var angle = collisionPointPortion * Mathf.PI;
+            var impulseX = Mathf.Cos(angle);
+            var impulseY = Mathf.Sin(angle);
+            rb2d.AddForce(new Vector2(impulseX, impulseY) * batImpulseMagnitude, ForceMode2D.Impulse);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("PlayerGoal"))
         {
             scoreTracker.incrementOpponentScore();
+            ResetBall();
         }
         if (collision.gameObject.CompareTag("OpponentGoal"))
         {
             scoreTracker.incrementPlayerScore();
+            ResetBall();
         }
-        ResetBall();
     }
 }
